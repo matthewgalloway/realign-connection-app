54cc99c62c9aecbe3a1b9861f08e397c7cff764b
Matthew Galloway <matthew.galloway@dataiku.com>
2024-08-24 16:18:49 +0100

    fix for deployment

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+echo "Current user: $(whoami)"
+echo "Current directory: $(pwd)"
+echo "Contents of /app:"
+ls -la /app
+echo "Permissions of start.sh:"
+ls -l /app/start.sh
+
 nginx -g "daemon off;" &
 . /opt/venv/bin/activate && cd /app/backend && /opt/venv/bin/flask run --host=0.0.0.0
